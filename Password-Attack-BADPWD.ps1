8de5145cca817d97cedb5599c63da133173e436d
Elli <36914463+eshlomo1@users.noreply.github.com>
2022-05-05 10:15:28 +0300

    Update Password-Attack-BADPWD.ps1

--- Password-Attack-BADPWD.ps1
+++ Password-Attack-BADPWD.ps1
@@ -1,29 +1,20 @@
 function Invoke-BadPWD{
-
     param (
         [Parameter(Mandatory=$true)]
         [string]$File,
         [string]$user
     )
-    
     if (!$File) {Write-Host "Invalid file..."; break}
-
     $Passwords = Get-Content $File
     $Error = $false
-    
      ForEach ($Password in $Passwords){
-     
      Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -ErrorAction SilentlyContinue -ErrorVariable ProcessError;
-     
      If ($ProcessError)
-        {
-            
-            Write-Host "Changing password for:... " $user : $password -ForegroundColor Red "[*] Failed"}
+        {   
+            Write-Host "Change password for:... " $user : $password -ForegroundColor Red "[*] Failed"}
     Else{
-
-            Write-Host "Changing password for:... " $user : $password -ForegroundColor Green "[*] Success"}
-    
-}}
+            Write-Host "Change password for:... " $user : $password -ForegroundColor Green "[*] Success"}
+    }}
 
 # Import-Module C:\Users\userid\BadPWD.ps1
-Invoke-BadPWD -File wordlist.txt -user userid
+#Invoke-BadPWD -File wordlist.txt -user userid
