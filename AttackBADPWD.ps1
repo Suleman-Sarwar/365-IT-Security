3ed5c7b63a5e090c4053472ab798755d51889a93
Elli <36914463+eshlomo1@users.noreply.github.com>
2022-05-05 10:27:19 +0300

    Update and rename Attack-BADPWD.ps1 to AttackBADPWD.ps1

---
+++ AttackBADPWD.ps1
@@ -0,0 +1,28 @@
+function Invoke-AttackBADPWD{
+
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$File,
+        [string]$user
+    )
+    
+    if (!$File) {Write-Host "Invalid file path..."; break}
+
+    $Passwords = Get-Content $File
+    $Error = $false
+    
+     ForEach ($Password in $Passwords){
+     
+     Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -ErrorAction SilentlyContinue -ErrorVariable ProcessError;
+     
+     If ($ProcessError)
+        {
+            
+            Write-Host "Change Password for: " $user : $password -ForegroundColor Red "[*] Failed!"}
+    Else{
+
+            Write-Host "Change password for: " $user : $password -ForegroundColor Green "[*] Success!"}
+    }}
+
+Import-Module C:\temp\AttackBADPWD.ps1 -Verbose
+Invoke-AttackBADPWD -File C:\temp\list.txt -user "elli.s@cloudefense.io" -Verbose
